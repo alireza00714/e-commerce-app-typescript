96d47784053e8ef9fc1344c9cdcaad7f053d34d2
Alireza Pirmozhdehi <zootel.land@gmail.comi>
2021-08-10 20:53:51 +0430

    Add some ux improvements

--- src/Components/CartItem/CartItem.tsx
+++ src/Components/CartItem/CartItem.tsx
@@ -4,7 +4,7 @@ import { FiPlus } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
 const CartItem = () => {
   return (
-    <div className="rounded-md border border-gray-300 hover:shadow-md transition-shadow flex flex-row-reverse">
+    <div className="rounded-md border border-gray-300 flex flex-row-reverse">
       <div className="w-32 lg:w-40 pr-1">
         <img
           className="w-full h-full cursor-pointer rounded-r-md"
@@ -16,7 +16,7 @@ const CartItem = () => {
         <span className="text-right text-sm lg:text-lg">
           پنیر لیقوان اوجان لبن مقدار 650 گرم
         </span>
-        <div className="flex flex-row-reverse justify-between">
+        <div className="flex flex-row-reverse items-center justify-between">
           <div className="flex flex-row-reverse">
             <div
               className={`${styles["cart-item__quantity"]} flex items-center rounded-md px-1 py-0.5 gap-3 ml-2`}
@@ -29,15 +29,8 @@ const CartItem = () => {
                 <FiPlus size="1.5rem" color="#9c203f" />
               </span>
             </div>
-            <div className="cursor-pointer flex items-center md:mr-2">
-              <span
-                className={`${styles["cart-item__delete-btn-text"]} text-xs font-md hidden md:block`}
-              >
-                حذف
-              </span>
-              <span>
-                <MdDelete size="1.5rem" fill="#9c203f" />
-              </span>
+            <div className="cursor-pointer flex items-center justify-center md:mr-2">
+              <MdDelete size="1.5rem" fill="#9c203f" />
             </div>
           </div>
           <div className="flex items-center gap-1">
